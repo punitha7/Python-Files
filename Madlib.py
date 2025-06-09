658d2312b61d57437cc76f8b3e642ef6666b335a
Punitha <punitha7@gmail.com>
2025-06-09 23:00:46 +0530

    Add files via upload

---
+++ Madlib.py
@@ -0,0 +1,9 @@
+adj = input("Adjective : ")
+verb1 = input("Verb1 : ")
+verb2 = input("Verb2 : ")
+hero = input("Famous Person : ")
+
+madlib = f"computer programming is so {adj}! it makes me so excited all the time because\
+            I love to {verb1}. Stay hyderated and {verb2} like you are a {hero}"
+
+print(madlib)
